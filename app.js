0dc049b43724f1da9c8125ff705c3f6bfb710e1e
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-21 23:21:56 +0530

    succesfully connected to the database.

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app  = express();
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const url = process.env.CONN;
+
+
+mongoose.connect(url)
+        .then(()=>{
+            console.log("Connected to the database succesfully.");
+        })
+        .catch((err)=>{
+            console.log("Error: ",err);
+        });
+
+
+
 
 
 app.listen(3000,(req,res)=>{
